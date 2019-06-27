50a057813d1021e64c494e161f57019460575815
Patrick <patrickthijssen27@gmail.com>
2019-06-27 20:42:42 +0200

    divide_by_zero tests; one does fail

--- pyflakes/test/test_interval_ast.py
+++ pyflakes/test/test_interval_ast.py
@@ -41,10 +41,45 @@ class TestBuiltins(TestCase):
         ''')
 
     def test_divide_by_zero(self):
-        self.flakes('''
-        def foo():
-            y = 0
-            x = 7
-            y = x // y
-        ''')
+        with self.assertRaises(ZeroDivisionError):
+            self.flakes('''
+            def foo():
+                y = 0
+                x = 7
+                y = x // y
+            ''')
 
+    def test_divide_by_zero_computed(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.flakes('''
+            def foo():
+                y = 1
+                x = 2
+                y = x // (y-1)
+            ''')
+
+    def test_divide_by_zero_floor(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.flakes('''
+            def foo():
+                y = 1
+                x = 2
+                y = x // (y // x)
+            ''')
+
+    def test_divide_by_zero_multiplication(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.flakes('''
+            def foo():
+                x = 2
+                y = x // (x * 0)
+            ''')
+
+    def test_divide_by_zero_addition(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.flakes('''
+            def foo():
+                x = -5
+                y = 5
+                y = 5 // (x + y)
+            ''')
