e2b9af071a60241c8e3345dce1c9a1c8df7159aa
Patrick <patrickthijssen27@gmail.com>
2019-06-30 16:44:23 +0200

    Check if warning is thrown in the correct way.

--- pyflakes/test/test_interval_ast.py
+++ pyflakes/test/test_interval_ast.py
@@ -2,7 +2,7 @@
 Tests for detecting redefinition of builtins.
 """
 from pyflakes.test.harness import TestCase
-from pyflakes.messages import DivisionByZero
+from pyflakes import messages as m
 
 
 class TestBuiltins(TestCase):
@@ -70,45 +70,40 @@ class TestBuiltins(TestCase):
         ''')
 
     def test_divide_by_zero(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                y = 0
-                x = 7
-                y = x // y
-            ''')
+        self.flakes('''
+        def foo():
+            y = 0
+            x = 7
+            y = x // y
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_computed(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                y = 1
-                x = 1
-                y = x // (y-x)
-            ''')
+        self.flakes('''
+        def foo():
+            y = 1
+            x = 1
+            y = x // (y-x)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_floor(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                y = 1
-                x = 2
-                y = x // (y // x)
-            ''')
+        self.flakes('''
+        def foo():
+            y = 1
+            x = 2
+            y = x // (y // x)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_multiplication(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                x = 2
-                y = x // (x * 0)
-            ''')
+        self.flakes('''
+        def foo():
+            x = 2
+            x = x // (x * 0)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_addition(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                x = -5
-                y = 5
-                y = 5 // (x + y)
-            ''')
+        self.flakes('''
+        def foo():
+            x = -5
+            y = 5
+            y = 5 // (x + y)
+        ''', m.DivisionByZero)
--- pyflakes/test/test_interval_ast_report.py
+++ pyflakes/test/test_interval_ast_report.py
@@ -2,95 +2,86 @@
 Tests for detecting redefinition of builtins.
 """
 from pyflakes.test.harness import TestCase
-from pyflakes.messages import DivisionByZero, DeadCode
+from pyflakes import messages as m
 
 
 class TestBuiltins(TestCase):
 
     def test_divide_by_zero(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                y = 0
-                x = 7
-                y = x // y
-            ''')
+        self.flakes('''
+        def foo():
+            y = 0
+            x = 7
+            y = x // y
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_addition(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                x = -5
-                y = 5
-                y = 5 // (x + y)
-            ''')
+        self.flakes('''
+        def foo():
+            x = -5
+            y = 5
+            y = 5 // (x + y)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_computed(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                y = 1
-                x = 1
-                y = x // (y-x)
-            ''')
+        self.flakes('''
+        def foo():
+            y = 1
+            x = 1
+            y = x // (y-x)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_multiplication(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                x = 2
-                y = x // (x * 0)
-            ''')
+        self.flakes('''
+        def foo():
+            x = 2
+            x = x // (x * 0)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_floor(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def foo():
-                y = 1
-                x = 2
-                y = x // (y // x)
-            ''')
+        self.flakes('''
+        def foo():
+            y = 1
+            x = 2
+            y = x // (y // x)
+        ''', m.DivisionByZero)
 
     def test_divide_by_zero_method(self):
-        with self.assertRaisesRegex(AssertionError, DivisionByZero.message):
-            self.flakes('''
-            def min(arg: int):
-                return arg - 1
-            
-            def foo():
-                1 // min(1)
-            ''')
+        self.flakes('''
+        def min(arg: int):
+            return arg - 1
+        
+        def foo():
+            1 // min(1)
+        ''', m.DivisionByZero)
 
     def test_dead_code_if(self):
-        with self.assertRaisesRegex(AssertionError, DeadCode.message):
-            self.flakes('''
-            def foo():
-                a = False
-                b = 1
-                if(a):
-                    b = b + b
-            ''')
+        self.flakes('''
+        def foo():
+            a = False
+            b = 1
+            if(a):
+                b = b + b
+        ''', m.DeadCode)
 
     def test_dead_code_elif(self):
-        with self.assertRaisesRegex(AssertionError, DeadCode.message):
-            self.flakes('''
-            def foo(arg: bool):
-                a = False
-                b = 1
-                if(arg):
-                    b = b + b
-                elif(a):
-                    b = b + b
-            ''')
+        self.flakes('''
+        def foo(arg: bool):
+            a = False
+            b = 1
+            if(arg):
+                b = b + b
+            elif(a):
+                b = b + b
+        ''', m.DeadCode)
 
     def test_dead_code_else(self):
-        with self.assertRaisesRegex(AssertionError, DeadCode.message):
-            self.flakes('''
-            def foo():
-                a = True
-                b = 1
-                if(a):
-                    b = b + b
-                else:
-                    b = b + b
-            ''')
+        self.flakes('''
+        def foo():
+            a = True
+            b = 1
+            if(a):
+                b = b + b
+            else:
+                b = b + b
+        ''', m.DeadCode)
