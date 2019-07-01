886ba74d86489570ace8e9660c8ea2e7cd085d83
Patrick <patrickthijssen27@gmail.com>
2019-07-01 10:00:50 +0200

    First comparator cases

---
+++ pyflakes/test/test_interval_comparator.py
@@ -0,0 +1,56 @@
+from pyflakes.interval import Interval, INFINITY, NEG_INFINITY, TOP, BOTTOM
+from pyflakes.boolean_lattice import TRUE, FALSE, BOOLEAN_TOP, BOOLEAN_BOTTOM
+from pyflakes.test.harness import TestCase
+
+
+class TestInterval(TestCase):
+    def assertEqual(self, first: Interval, second: Interval, msg=...) -> None:
+        super(TestInterval, self).assertEqual(str(first), str(second), msg)
+
+    def test_smaller(self):
+        self.assertEqual(Interval(5) < Interval(6), TRUE)
+        self.assertEqual(Interval(-1) < Interval(1), TRUE)
+        self.assertEqual(Interval(0) < Interval(-1), FALSE)
+        self.assertEqual(Interval(-1) < Interval(1), FALSE)
+        self.assertEqual(Interval(1) < Interval(1), FALSE)
+
+    def test_smaller_interval(self):
+        self.assertEqual(Interval(1, 2) < Interval(5, 6), TRUE)
+        self.assertEqual(Interval(5, 6) < Interval(1, 2), FALSE)
+        self.assertEqual(Interval(2, 5) < Interval(4, 6), BOOLEAN_TOP)
+        self.assertEqual(Interval(2, 5) < Interval(3, 4), BOOLEAN_TOP)
+        self.assertEqual(Interval(3, 4) < Interval(2, 5), BOOLEAN_TOP)
+
+    def test_greater(self):
+        self.assertEqual(Interval(5) > Interval(6), FALSE)
+        self.assertEqual(Interval(-1) > Interval(1), FALSE)
+        self.assertEqual(Interval(0) > Interval(-1), TRUE)
+        self.assertEqual(Interval(-1) > Interval(1), TRUE)
+        self.assertEqual(Interval(1) > Interval(1), FALSE)
+
+    def test_greater_interval(self):
+        self.assertEqual(Interval(1, 2) > Interval(5, 6), FALSE)
+        self.assertEqual(Interval(5, 6) > Interval(1, 2), TRUE)
+        self.assertEqual(Interval(2, 5) > Interval(4, 6), BOOLEAN_TOP)
+        self.assertEqual(Interval(2, 5) > Interval(3, 4), BOOLEAN_TOP)
+        self.assertEqual(Interval(3, 4) > Interval(2, 5), BOOLEAN_TOP)
+
+    def test_equals(self):
+        self.assertEqual(Interval(0) == Interval(0), TRUE)
+        self.assertEqual(Interval(-1) == Interval(1), FALSE)
+        self.assertEqual(Interval(0) == Interval(-1), FALSE)
+
+    def test_equals_interval(self):
+        self.assertEqual(Interval(1, 3) == Interval(1, 3), BOOLEAN_TOP)
+        self.assertEqual(Interval(1, 2) == Interval(2, 3), BOOLEAN_TOP)
+        self.assertEqual(Interval(1, 2) == Interval(3, 5), FALSE)
+
+    def test_nequals(self):
+        self.assertEqual(Interval(0) != Interval(0), FALSE)
+        self.assertEqual(Interval(-1) != Interval(1), TRUE)
+        self.assertEqual(Interval(0) != Interval(-1), TRUE)
+
+    def test_nequals_interval(self):
+        self.assertEqual(Interval(1, 3) != Interval(1, 3), BOOLEAN_TOP)
+        self.assertEqual(Interval(1, 2) != Interval(2, 3), BOOLEAN_TOP)
+        self.assertEqual(Interval(1, 2) != Interval(3, 5), TRUE)
